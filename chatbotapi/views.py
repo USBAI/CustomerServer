a2b8f806547fc68639fe84afa6d0cadadae7f31f
USBAI <usbai.contact@gmail.com>
2024-08-01 16:39:39 +0200

    Update views.py

--- chatbotapi/views.py
+++ chatbotapi/views.py
@@ -32,6 +32,8 @@ def chatbot_api(request):
                 Pay close attention to details in the conversation. If the user expresses interest in buying something in the fashion category, understand the product they want and ask them for a specific price range if they don't provide one. If they provide a product name, include the product name in your response using the format ((product name)). If they provide a price range, include the price in the format [[price]]. Do not ask the user about brands or any technical details related to computer programming code.
 
                 When you have found products that match the user's request, respond with the top 10 product information directly without stating that you are looking for it. Use the following format for each product: "Here is a product I found for you: ((product name)) for [[price]]. You can buy it [here]((product URL)). Description: ((product description))." Ensure to provide all product details in your response.
+
+                respond in html format dont include styling name all the a links id to be products links
             '''
 
             # Create the completion using GPT-4
