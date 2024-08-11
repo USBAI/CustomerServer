e48af6518ff0aab8aed3b5812e16067c070e9781
USBAI <usbai.contact@gmail.com>
2024-08-11 17:34:41 +0200

    Implemented new feature api

--- AddCart/urls.py
+++ AddCart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_to_cart, get_cart
+from .views import add_to_cart, get_cart, remove_from_cart
 
 urlpatterns = [
     path('add-to-cart/', add_to_cart, name='add-to-cart'),
     path('getcart/', get_cart, name='add-to-cart'),
+    path('remove-from-cart/', remove_from_cart, name='remove_from_cart'), 
 ]
--- AddCart/views.py
+++ AddCart/views.py
@@ -87,3 +87,48 @@ def get_cart(request):
             return Response({"message": "Cart is empty"}, status=200)
     else:
         return Response({"message": "User not found"}, status=404)
+
+
+
+
+
+@api_view(['POST'])
+def remove_from_cart(request):
+    # Extract the user_id and product_url from the request
+    user_id = request.data.get('user_id')
+    product_url = request.data.get('product_url')
+
+    # Reference to the 'Kluret_Users' in Firebase
+    ref = db.reference('All_Users/Kluret_Users')
+
+    # Find the user by user_id
+    users_snapshot = ref.get()
+    matching_user = None
+
+    for email_key, user_data in users_snapshot.items():
+        if user_data.get('User-ID') == user_id:
+            matching_user = email_key
+            break
+
+    if matching_user:
+        # Reference to the 'MyCart' collection under the user's node
+        cart_ref = ref.child(f"{matching_user}/MyCart")
+        cart_items = cart_ref.get()
+
+        if cart_items:
+            item_to_remove = None
+            for key, value in cart_items.items():
+                if value.get('product_url') == product_url:
+                    item_to_remove = key
+                    break
+
+            if item_to_remove:
+                # Remove the item from the cart
+                cart_ref.child(item_to_remove).delete()
+                return Response({"message": "Item removed from cart successfully"}, status=200)
+            else:
+                return Response({"message": "Item not found in cart"}, status=404)
+        else:
+            return Response({"message": "Cart is empty"}, status=404)
+    else:
+        return Response({"message": "User not found"}, status=404)
