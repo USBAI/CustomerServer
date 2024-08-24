c532ff63b8fcc6f134b0d8cf240b5dcb561e215c
USBAI <usbai.contact@gmail.com>
2024-08-24 04:48:06 +0200

    first commit

--- usbai_client/settings.py
+++ usbai_client/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "http://localhost:3002",
     "https://usbai.org",
     'https://www.kluret.se',
     "https://usbai.vercel.app",
